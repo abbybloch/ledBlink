61301be80fba2424bfc790c88aaed3132bfc1ec0
abbybloch <apb465@nyu.edu>
2021-11-21 17:12:39 -0500

    added a button to trigger the LED blink

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,14 +1,19 @@
-
+//initialize variables
 int ledPin = 13;
+int buttonPin = 14;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  //setup a button
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
-  
+  //if button is pressed, blink the led
+  if (digitalRead(buttonPin) == HIGH){
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  } 
 }
